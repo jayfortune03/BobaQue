37866bf446aa68b93136de7384b61bf2ddde4a08
Herjuno Pangestu <herjunopangestu@gmail.com>
2021-07-29 01:56:25 +0700

    add homepage and navbar

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,20 @@
+const { Menu, Topping } = require('../models')
+
 class HomeController {
     static viewPage(req, res) {
         // res.render(`register`)
         // console.log(req.session)
-        res.send(`Testing Masyok`)
+        let menu
+        Menu.findAll()
+            .then(data => {
+                menu = data
+                return Topping.findAll()
+            })
+            .then(data => {
+                res.render(`homepage`, {data,menu})
+            })
+            .catch(err => res.send(err))
+        
     }
 }
 
