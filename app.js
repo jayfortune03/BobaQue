17dd91acb20ec0992079e059b91241c23d2f515c
Nicholas Fortune <nfortune03@gmail.com>
2021-07-30 04:46:28 +0700

    revisi

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 const router = require(`./routes/index.js`)
 const session = require(`express-session`)
 
