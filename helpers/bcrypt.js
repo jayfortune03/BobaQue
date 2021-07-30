b744159755d2912113e8394c0f81619fe65fe529
Nicholas Fortune <nfortune03@gmail.com>
2021-07-30 10:03:45 +0700

    push test

--- helpers/bcrypt.js
+++ helpers/bcrypt.js
@@ -15,3 +15,6 @@ module.exports = {
 }
 
 // console.log(hashPassword(`pass12345`))
+
+// console.log(hashPassword(`bobaque123`))
+// console.log(checkPassword(`bobaque123`, `$2a$10$B6VhW/a7s5ju2eqgM/BKFOrR1LW.AHfHC.erAfuyXf3OA6emSlCEu`))
